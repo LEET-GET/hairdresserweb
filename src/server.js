eb27644e06d32c8b2239fa9c25a279cd3b1dc1bb
LEET-GET <96512352+LEET-GET@users.noreply.github.com>
2024-01-15 15:36:36 +0600

    server.js

--- src/server.js
+++ src/server.js
@@ -186,7 +186,7 @@ app.post('/edit-booking', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
